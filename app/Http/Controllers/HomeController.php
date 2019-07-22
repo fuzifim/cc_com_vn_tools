fc8a61f9b2fcce4e63ea08b8c54abfd5a899714d
Nguyen Thanh <nguyen.thanh@alliedtechcamp.com>
2019-07-22 15:54:50 +0700

    fix memcache

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,13 +11,10 @@ class HomeController extends Controller
 {
     public function index(Request $request){
         $page = $request->has('page') ? $request->query('page') : 1;
-        $newDomain=Cache::store('memcached')->remember('newDomain_'.$page, 1, function()
-        {
-            return DB::connection('mongodb')->collection('mongo_domain')
-                ->where('craw_next','exists',true)
-                ->orderBy('updated_at','desc')
-                ->simplePaginate(50);
-        });
+        $newDomain=DB::connection('mongodb')->collection('mongo_domain')
+            ->where('craw_next','exists',true)
+            ->orderBy('updated_at','desc')
+            ->simplePaginate(50);
         $data=[
             'newDomain'=>$newDomain
         ];
@@ -25,11 +22,8 @@ class HomeController extends Controller
     }
     public function show($slug){
         if(!empty($slug)){
-            $domain = Cache::store('memcached')->remember('infoDomain_'.base64_encode($slug), 1, function() use($slug)
-            {
-                return DB::connection('mongodb')->collection('mongo_domain')
-                    ->where('base_64',base64_encode($slug))->first();
-            });
+            $domain = DB::connection('mongodb')->collection('mongo_domain')
+                ->where('base_64',base64_encode($slug))->first();
             if(!empty($domain['domain'])){
                 DB::connection('mongodb')->collection('mongo_domain')
                     ->where('base_64',base64_encode($slug))
